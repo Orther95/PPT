b3754319fbb851aa574f68428e3a05c011c738cb
Orther95 <Juanra.95@hotmail.es>
2022-02-07 18:22:52 +0100

    Commit 3

--- PPT_GAME.py
+++ PPT_GAME.py
@@ -12,3 +12,19 @@ if idioma == 1:
     print("2)Papel")
     print("3)Tijera")
     opcion = int(input("Que elijes: "))
+    if opcion == 1:
+        elijeUsuario = "piedra"
+    elif opcion == 2:
+        elijeUsuario = "papel"
+    elif opcion == 3:
+        elijeUsuario = "tijera"
+    print("Tu elijes: ", elijeUsuario)
+    
+    if aleatorio == 0:
+        elijePc = "piedra"
+    elif aleatorio == 1:
+        elijePc = "papel"
+    elif aleatorio == 2:
+        elijePc = "tijera"
+    print("PC elijio: ", elijePc)
+    print("...")
